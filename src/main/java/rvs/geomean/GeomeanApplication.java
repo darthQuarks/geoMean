0701328019792fc32f1c9b538190bffc364aa6f5
Rasmus Vestergaard Skannrup <git@quarks.dk>
2023-05-19 12:17:47 +0200

    Initial setup with spring boot

---
+++ src/main/java/rvs/geomean/GeomeanApplication.java
@@ -0,0 +1,13 @@
+package rvs.geomean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeomeanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeomeanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rvs/geomean/GeomeanApplicationTests.java
@@ -0,0 +1,13 @@
+package rvs.geomean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeomeanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
