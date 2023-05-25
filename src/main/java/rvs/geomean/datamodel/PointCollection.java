d153af59a256665907b249785b20857e37623fb0
Rasmus Vestergaard Skannrup <git@quarks.dk>
2023-05-25 11:40:47 +0200

    Clean up PointCollection class for unused methods

--- src/main/java/rvs/geomean/datamodel/PointCollection.java
+++ src/main/java/rvs/geomean/datamodel/PointCollection.java
@@ -5,27 +5,8 @@ import java.util.List;
 
 public class PointCollection extends ArrayList<Point> implements List<Point> {
 
-    int currentIndex = 0;
-
-    public Point getLast() {
-        if (size() > 0) {
-            return get(size() - 1);
-        }
-        return null;
-    }
-
-    public Point getNext() {
-        if (currentIndex >= size()) {
-            return null;
-        }
-        return get(currentIndex++);
-    }
-
-    public int getCurrentIndex() {
-        return currentIndex;
-    }
-
-    public void resetCurrentIndex() {
-        currentIndex = 0;
+    public PointCollection append(Point point) {
+        add(point);
+        return this;
     }
 }
