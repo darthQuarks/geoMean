fc43c054bd4b34b8407c640864396ad46185d726
Rasmus Vestergaard Skannrup <git@quarks.dk>
2023-05-25 11:53:44 +0200

    renamerino

--- src/test/java/rvs/geomean/GeomeanApplicationTests.java
+++ src/test/java/rvs/geomean/GeomeanApplicationTests.java
@@ -25,15 +25,13 @@ class GeomeanApplicationTests {
 		Point point1 = new Point(0, 10);
 		Point point2 = new Point(10, 0);
 
-		Point result0 = new Point(0, 5);
-		Point result1 = new Point(0, 1);
-		Point result2 = new Point(5, 0);
+		Point expected = new Point(0, 5);
+        Point actual = meanPointService.getMeanPoint(point0, point1, 1);
+        Assert.isTrue(actual.pretty().equals(expected.pretty()), "Point are not equal to scale: " + Point.getScale() + ", east-west calculation is wrong");
 
-        Point meanPoint = meanPointService.getMeanPoint(point0, point1, 1);
-        Assert.isTrue(meanPoint.pretty().equals(result0.pretty()), "Point are not equal to scale: " + Point.getScale() + ", east-west calculation is wrong");
-
-        meanPoint = meanPointService.getMeanPoint(point0, point2, 1);
-        Assert.isTrue(meanPoint.pretty().equals(result2.pretty()), "Point are not equal to scale: " + Point.getScale() + ", north-south calculation is wrong");
+        expected = new Point(5, 0);
+        actual = meanPointService.getMeanPoint(point0, point2, 1);
+        Assert.isTrue(actual.pretty().equals(expected.pretty()), "Point are not equal to scale: " + Point.getScale() + ", north-south calculation is wrong");
     }
 
     @Test
@@ -42,17 +40,17 @@ class GeomeanApplicationTests {
         Point point0 = new Point(0, 0);
         Point point1 = new Point(0, 10);
 
-        Point result = new Point(0, 1);
-        Point meanPoint = meanPointService.getMeanPoint(point0, point1, 9);
-        Assert.isTrue(meanPoint.pretty().equals(result.pretty()), "Point are not equal to scale: " + Point.getScale() + ", mass calculation is wrong");
+        Point expected = new Point(0, 1);
+        Point actual = meanPointService.getMeanPoint(point0, point1, 9);
+        Assert.isTrue(actual.pretty().equals(expected.pretty()), "Point are not equal to scale: " + Point.getScale() + ", mass calculation is wrong");
 
-        result = new Point(0, 2);
-        meanPoint = meanPointService.getMeanPoint(point0, point1, 4);
-        Assert.isTrue(meanPoint.pretty().equals(result.pretty()), "Point are not equal to scale: " + Point.getScale() + ", mass calculation is wrong");
+        expected = new Point(0, 2);
+        actual = meanPointService.getMeanPoint(point0, point1, 4);
+        Assert.isTrue(actual.pretty().equals(expected.pretty()), "Point are not equal to scale: " + Point.getScale() + ", mass calculation is wrong");
 
-        result = new Point(0, 3.333333333333333);
-        meanPoint = meanPointService.getMeanPoint(point0, point1, 2);
-        Assert.isTrue(meanPoint.pretty().equals(result.pretty()), "Point are not equal to scale: " + Point.getScale() + ", mass calculation is wrong");
+        expected = new Point(0, 3.333333333333333);
+        actual = meanPointService.getMeanPoint(point0, point1, 2);
+        Assert.isTrue(actual.pretty().equals(expected.pretty()), "Point are not equal to scale: " + Point.getScale() + ", mass calculation is wrong");
     }
 
     @Test
